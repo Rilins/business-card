fa0a6ea224a377738d712652f7c37280868c6466
Érica Lins <linsericadev@gmail.com>
2024-02-16 01:29:17 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,14 @@
+![](./assets/tela.png)
+
+### Tecnologias
+Esse projeto foi desenvolvido com as tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git Github
+
+## Projeto
+Página de links para usar como cartão de visitas online.
+
+## Licença
+Esse projeto está sob a Licença MIT.
---
+++ script.js
@@ -0,0 +1,12 @@
+function mudarModo() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#perfil img")
+
+    /*if(html.classList.contains("light")) {
+        img.setAttribute('src,' './assets/')
+    }else {
+        img.setAttribute('src,' './assets/')
+    }*/
+}
